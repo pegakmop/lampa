2086a8c5be2c1c9d691333125b9900b06e59c7e0
pegakmop <93389368+pegakmop@users.noreply.github.com>
2026-03-07 02:35:38 +0300

    Update modification.js

--- plugins/modification.js
+++ plugins/modification.js
@@ -65,6 +65,8 @@
         Lampa.Storage.set('jackett_url','https://sw.ext.io/jac');
         Lampa.Storage.set('jackett_key','1');
         Lampa.Storage.set('parser_torrent_type','jackett');
+		Lampa.Storage.set('torrserver_url','http://37.192.106.55:8095');
+		Lampa.Storage.set('torrserver_url_two','http://ts.maxvol.pro');
 		
         var unic_id = Lampa.Storage.get('lampac_unic_id', '');
 
