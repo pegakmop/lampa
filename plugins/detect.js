8f73d61aac16e37895d777306c2b150b02432c0a
pegakmop <93389368+pegakmop@users.noreply.github.com>
2026-03-16 22:49:35 +0300

    Update detect.js

--- plugins/detect.js
+++ plugins/detect.js
@@ -45,6 +45,6 @@
 
         if ('serviceWorker' in navigator) {
             window.addEventListener('load', () => {
-                navigator.serviceWorker.register('./index.js').then(reg => console.log('ServiceWorker registered:', reg.scope)).catch(err => console.log('ServiceWorker registration failed:', err));
+                navigator.serviceWorker.register('index.js').then(reg => console.log('ServiceWorker registered:', reg.scope)).catch(err => console.log('ServiceWorker registration failed:', err));
             });
         }
